4974a6dfad5d15e3f334ed8b6fd135f3861f3fa0
Umang Verma <umang.verma@oodles.io>
2022-06-16 14:46:47 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+var webpack = require("webpack");
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        // exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.mp3$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv(),
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+    new HtmlWebpackPlugin({
+      title: "Communication Scaffold: Quality Video Collaboration Solution",
+      favicon: "public/favicon.ico",
+      template: "./public/index.html",
+    }),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+    new webpack.ExternalsPlugin("commonjs", ["electron"]),
+    new webpack.IgnorePlugin({ resourceRegExp:/vertx/, contextRegExp:/ajv/ }),
+    new NodePolyfillPlugin({
+			excludeAliases: ["console"]
+		})
+  ],
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+    alias: {
+      process: "process/browser",
+    },
+  },
+};
---
+++ webpack.prod.js
@@ -0,0 +1,43 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config.js');
+const path = require('path');
+
+module.exports = env => {
+  let baseUrl = '';
+  if (env.dev){
+    baseUrl = 'https://dev.communication-scaffold.oodleslab.com/';
+  }else if (env.stage){
+    baseUrl = 'https://stage.communication-scaffold.oodleslab.com/';
+  }else if (env.prod){
+    baseUrl = 'https://communication-scaffold.oodles.io/';
+  }
+
+  return merge(common, {
+    mode: 'production',
+    entry: ['babel-polyfill', path.resolve(__dirname, './src/main.js')],
+    module: {
+      rules: [
+        {
+          test: /\.png|woff|woff2|eot|ttf|svg|jpg|gif$/,
+          loader: 'file-loader',
+          options: {
+            outputPath: 'chat-app/images',
+            publicPath: baseUrl + 'chat-app/images',
+          },
+        }
+      ]
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'chat-app/js/main.js',
+      publicPath: "/",
+      clean: true
+    },
+    stats: {
+      logging: true,
+      errors: true,
+      errorDetails: true,
+      timings: true
+    }
+  })
+};
