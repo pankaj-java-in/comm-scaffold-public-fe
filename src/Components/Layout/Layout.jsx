eba49e58001a79706ea138db432dcf002dd4a2ac
Umang Verma <umang.verma@oodles.io>
2022-06-16 19:28:18 +0530

    added helpng component

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -4,12 +4,14 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Features from "../Body/Features";
 import ShowFaq from "../FAQ/ShowFaq";
+import HelpingIndustries from "../Body/HelpingIndustries";
 
 const Layout = () => {
   return (
     <div className="Layout">
         <Header/>
         <Features/>
+        <HelpingIndustries/>
         <ShowFaq/>
         <Footer/>
     </div>
