7e8dc113929a6d213a7484370c3300ddd9b69c0f
Umang Verma <umang.verma@oodles.io>
2022-06-17 15:44:43 +0530

    somwhting

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,6 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
 const path = require('path');
 
-
 module.exports = env => {
   let baseUrl = 'https://stage.communication-scaffold.oodleslab.com/';
   // if (env.dev){
