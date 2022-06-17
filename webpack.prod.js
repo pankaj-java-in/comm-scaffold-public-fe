f8472f9f411b58b68ee9f673bd1d492daf848c66
Umang Verma <umang.verma@oodles.io>
2022-06-17 15:41:07 +0530

    somwhting

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 
 module.exports = env => {
-  let baseUrl = 'https://stage.communication-scaffold.oodleslab.com/public';
+  let baseUrl = 'https://stage.communication-scaffold.oodleslab.com/';
   // if (env.dev){
   //   baseUrl = 'https://ms.oodleslab.com/chat/';
   // }else if (env.stage){
