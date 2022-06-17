875ec1fdd5679704eb034d45b0249c3b52f1da11
Umang Verma <umang.verma@oodles.io>
2022-06-17 19:16:03 +0530

    sss

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./Layout.scss";
-// import Header from "../Header/Header";
-// import Footer from "../Footer/Footer";
-// import Features from "../Body/Features";
-// import ShowFaq from "../FAQ/ShowFaq";
-// import HelpingIndustries from "../Body/HelpingIndustries";
-// import Implementation from "../Body/Implementation";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Features from "../Body/Features";
+import ShowFaq from "../FAQ/ShowFaq";
+import HelpingIndustries from "../Body/HelpingIndustries";
+import Implementation from "../Body/Implementation";
 
 const Layout = () => {
   return (
     <div className="Layout">
-       hello
+        <Header/>
+        <Features/>
+        <Implementation/>
+        <HelpingIndustries/>
+        <ShowFaq/>
+        <Footer/>
     </div>
   );
 };
